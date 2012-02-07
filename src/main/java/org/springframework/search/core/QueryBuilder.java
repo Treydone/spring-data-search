197e21fea894abe2de9a16b0e5aa234a4ca54df1
Vincent Devillers <vdevillers76@gmail.com>
2012-02-07 16:17:36 +0100

    Same commit...

---
+++ src/main/java/org/springframework/search/core/QueryBuilder.java
@@ -0,0 +1,45 @@
+package org.springframework.search.core;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.springframework.search.InvalidParamsException;
+
+public abstract class QueryBuilder {
+
+	private static final Pattern NAMES_PATTERN = Pattern.compile("\\{\\w+\\}");
+
+	private static String getVariableValueAsString(Object variableValue) {
+		return variableValue != null ? variableValue.toString() : "";
+	}
+
+	public static String resolveParams(String query, Object[] params) {
+		if (!ArrayUtils.isEmpty(params)) {
+			Iterator<Object> iterator = Arrays.asList(params).iterator();
+			Matcher matcher = NAMES_PATTERN.matcher(query);
+
+			int i = 0;
+			StringBuffer sb = new StringBuffer(query.length());
+			while (matcher.find()) {
+				i++;
+				if (params.length < i) {
+					throw new InvalidParamsException("Some parameters are missing:" + Arrays.toString(params));
+				}
+				Object variableValue = iterator.next();
+				String variableValueString = getVariableValueAsString(variableValue);
+				String replacement = Matcher.quoteReplacement(variableValueString);
+				matcher.appendReplacement(sb, replacement);
+			}
+			matcher.appendTail(sb);
+			
+			if (params.length > i) {
+				throw new InvalidParamsException("Too much parameters for this query!" + Arrays.toString(params));
+			}
+			query = sb.toString();
+		}
+		return query;
+	}
+}
---
+++ src/test/java/org/springframework/search/core/TestQueryBuilder.java
@@ -0,0 +1,37 @@
+package org.springframework.search.core;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.springframework.search.InvalidParamsException;
+
+public class TestQueryBuilder {
+
+	@Test(expected = InvalidParamsException.class)
+	public void searchWithNoEnoughParams() {
+		QueryBuilder.resolveParams("id:{id} and name:{name}", new Object[] { 123 });
+	}
+
+	@Test(expected = InvalidParamsException.class)
+	public void searchWithToMuchParams() {
+		QueryBuilder.resolveParams("id:{id}", new Object[] { 123, "toto" });
+	}
+
+	@Test
+	public void searchWithOneParam() {
+		String result = QueryBuilder.resolveParams("id:{id}", new Object[] { 123 });
+		assertEquals("id:123", result);
+	}
+
+	@Test
+	public void searchWithManyParams() {
+		String result = QueryBuilder.resolveParams("id:{id} and name:{name} or else", new Object[] { 123, "toto" });
+		assertEquals("id:123 and name:toto or else", result);
+	}
+	
+	@Test
+	public void jsonSearchWithManyParams() {
+		String result = QueryBuilder.resolveParams("{\"query\" : {\"field\" : { \"id\" : \"{id}\"}}}", new Object[] { 123 });
+		assertEquals("{\"query\" : {\"field\" : { \"id\" : \"123\"}}}", result);
+	}
+}
